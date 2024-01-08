eb4de619717c3f19be3ef6892d9d81c733d23d02
anirudh <anirudhce@gmail.com>
2024-01-07 21:30:14 -0500

    changes 10

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
@@ -1,4 +1,5 @@
 package uk.co.danielbryant.djshopping.stockmanager;
+ 
 import java.util.Date;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class healthcheck {
     @GetMapping("/healthcheckSM")
     public String healthcheck() {
-        return "Hello User this response is from ShopFront!! " + new Date();
+        return "Hello User this response from Stock Manager !! " + new Date();
     }
 }
